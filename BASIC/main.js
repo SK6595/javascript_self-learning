a3d53e63057a886002eee11be51cafe9ef09cbf7
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-28 14:09:49 +0000

    「変数、定数の名前のルール」学習完了

--- BASIC/main.js
+++ BASIC/main.js
@@ -1,4 +1,13 @@
 let count = 0;
 count = 30;
 let newCount = 0;
-const daysInWeek = 7;
+const daysInWeek =7;
+let tomatoCount;
+let tomatocount;
+let $tomato$Count;
+let _tomato_Count;
+let tomato7Count;
+let 7tomatoCount;
+let トマトカウント;
+let tomato_count;
+let Tomato_count;
