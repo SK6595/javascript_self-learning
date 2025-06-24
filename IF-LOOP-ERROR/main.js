2d312d924949286be2f26878bc0dfd4f8e5bb65b
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-06-24 23:52:53 +0000

    「===と==の違い」学習途中

--- IF-LOOP-ERROR/main.js
+++ IF-LOOP-ERROR/main.js
@@ -1,9 +1,15 @@
-let ok = false;
+let ok = true;
 let maybeOk = false;
-if (ok){
-  console.log('OK!');
+if (ok) {
+  // console.log('OK!');
 } else if (maybeOk) {
   console.log('maybe OK...');
 } else {
   console.log('NO!');
 }
+ok = 1 === 1;
+ok = 'hello' === 'hello';
+ok = 1 !== 2; /*「!==」は、左の項と右の項が違うとtrueになる。*/
+ok = 1 == 1;
+ok = 1 === '1';
+console.log(ok);
