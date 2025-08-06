84ec954afd91a7ec097bfa03b33c094fd456b5c8
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-08-06 06:47:31 +0000

    「for-of文」学習完了

--- IF-LOOP-ERROR/main.js
+++ IF-LOOP-ERROR/main.js
@@ -115,9 +115,9 @@ for (let pumpkinCount = 0, i = 0; pumpkinCount < 10; pumpkinCount += 1, i += 1){
 //letの中のカンマもlet宣言の一つになる。最後の式のカンマは、「カンマ演算子」になる。カンマ演算子は複数の式を1つの式にまとめるようなものになる。
 
 const fruits = ['apple', 'banana', 'grape', 'orange', 'mango'];
-for (let i = 0; i < fruits.length; i += 1) {
+for (let i = 0; i < fruits.length; i += 1) { //i < fruits.lengthと書くことで、配列の数を教えてくれる。
   console.log(fruits[i]);
 }
-for (const fruit of fruits) {
-  console.log(fruit);
-}
+for (const fruit of fruits) { //for-of文は、配列をループするための文になる。あまり配列の中の値を書き換えることはないので、基本的にはconstで書くことがすごく一般的である。
+  console.log(fruit); //「fruit」の箇所は、他のfor文やwhile文とは違い、1回1回削除されているイメージで、毎回毎回ループごとに違うブロック文の中で実行している（使用している）というイメージを持つこと。
+} 
