8bf2d71c37f8000594446d35d665f9dc49aadd25
SK6595 <173334721+SK6595@users.noreply.github.com>
2026-08-14 07:32:32 +0000

    「chapter3：HTML要素の取得、JavaScriptから文字を表示させる、DOM、カラーコードの表示、テンプレート文字列」学習完了

--- JavaScript/JavaScript5/ColorPicker/js/script.js
+++ JavaScript/JavaScript5/ColorPicker/js/script.js
@@ -1,1 +1,3 @@
-console.log('準備完了!');
+// 最終的に表示したい内容を全てバックティック（バッククォート）で囲む。
+// 式・値・変数・定数など、文字列以外のコードは、${}で囲む。
+document.querySelector('#colorText').textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
